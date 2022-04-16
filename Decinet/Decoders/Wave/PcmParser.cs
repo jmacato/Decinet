54d96ec1b8ca06536b126f2de5baa32f80f8d2b5
Jumar Macato <16554748+jmacato@users.noreply.github.com>
2022-04-16 19:45:59 +0800

    fix streaming logic for PcmParser.cs

--- Decinet/Decoders/Wave/PcmParser.cs
+++ Decinet/Decoders/Wave/PcmParser.cs
@@ -7,80 +7,80 @@ namespace Decinet.Decoders.Wave;
 
 internal class PcmParser : WaveParser
 {
-    private readonly Format _format;
-
     public PcmParser(BinaryReader currentStream, WaveFormat format, long rawDataStartOffset) :
         base(currentStream, format, rawDataStartOffset)
     {
-        _format = Format.BitsPerSample switch
+        AudioFormat = Format.BitsPerSample switch
         {
-            16 => new Format(typeof(short), sizeof(short), (int) format.SampleRate, format.NumChannels),
-            32 => new Format(typeof(float), sizeof(float), (int) format.SampleRate, format.NumChannels),
-            64 => new Format(typeof(long), sizeof(long), (int) format.SampleRate, format.NumChannels),
+            16 => new AudioFormat(typeof(short), sizeof(short), (int) format.SampleRate, format.NumChannels),
+            32 => new AudioFormat(typeof(float), sizeof(float), (int) format.SampleRate, format.NumChannels),
+            64 => new AudioFormat(typeof(long), sizeof(long), (int) format.SampleRate, format.NumChannels),
             _ => throw new Exception("Invalid bit size.")
         };
     }
 
+    public override AudioFormat AudioFormat { get; }
 
-    public override bool TryGetBytes(int numSamples, out ISampleFrame? sampleFrame)
+    public override bool TryGetBytes(int numSamples, out ISampleFrame sampleFrame)
     {
-        sampleFrame = null;
+        var samplesToGet = numSamples;
+        sampleFrame = null!;
         var bytesPerSample = Format.BitsPerSample / 8;
-        var sampleFrameBytes = numSamples * Format.NumChannels * (bytesPerSample);
+        var sampleFrameBytes = samplesToGet * Format.NumChannels * (bytesPerSample);
 
         var normalizedSeekScalar = CurrentPosition.TotalSeconds / Duration.TotalSeconds;
         var byteOffset = (long) (normalizedSeekScalar * TotalRawDataBytes) + RawDataStartOffset;
-        var streamPos = CurrentStream.BaseStream.Position;
-        var streamLength = CurrentStream.BaseStream.Length;
+        var streamPosBytes = CurrentStream.BaseStream.Position;
+        var streamLengthBytes = CurrentStream.BaseStream.Length;
 
-        if (streamPos + sampleFrameBytes > streamLength)
+        if (byteOffset + sampleFrameBytes >= streamLengthBytes)
         {
-            var remainingSamplesBytes = (int) (streamPos + sampleFrameBytes) - (int) streamLength;
+            var remainingSamplesBytes = (int) (streamLengthBytes - byteOffset);
             if (remainingSamplesBytes == 0)
             {
                 return false;
             }
             else
             {
-                numSamples = remainingSamplesBytes / Format.NumChannels / bytesPerSample;
+                samplesToGet = remainingSamplesBytes / Format.NumChannels / bytesPerSample;
                 sampleFrameBytes = remainingSamplesBytes;
             }
         }
 
         CurrentStream.BaseStream.Position = byteOffset;
-        
+
         switch (bytesPerSample)
         {
             case sizeof(short):
-                var rawShort = ArrayPool<short>.Shared.Rent(Format.NumChannels * numSamples);
+                var rawShort = ArrayPool<short>.Shared.Rent(Format.NumChannels * samplesToGet);
 
-                for (var i = 0; i < sampleFrameBytes / sizeof(short); i++)
+                for (var i = 0; i < samplesToGet; i++)
                 {
                     rawShort[i] = CurrentStream.ReadInt16();
                 }
 
-                sampleFrame = new ShortSampleFrame(rawShort, numSamples, Format.NumChannels, _format);
+                sampleFrame = new ShortSampleFrame(rawShort, samplesToGet, Format.NumChannels, AudioFormat);
                 break;
             case sizeof(float):
-                var rawFloat = ArrayPool<float>.Shared.Rent(Format.NumChannels * numSamples);
+                var rawFloat = ArrayPool<float>.Shared.Rent(Format.NumChannels * samplesToGet);
 
-                for (var i = 0; i < sampleFrameBytes / sizeof(float); i++)
+                for (var i = 0; i < samplesToGet; i++)
                 {
                     rawFloat[i] = CurrentStream.ReadSingle();
                 }
 
-                sampleFrame = new FloatSampleFrame(rawFloat, numSamples, Format.NumChannels, _format);
+                sampleFrame = new FloatSampleFrame(rawFloat, samplesToGet, Format.NumChannels, AudioFormat);
                 break;
             case sizeof(double):
 
-                var rawDouble = ArrayPool<double>.Shared.Rent(Format.NumChannels * numSamples);
+                var rawDouble = ArrayPool<double>.Shared.Rent(Format.NumChannels * samplesToGet);
 
-                for (var i = 0; i < sampleFrameBytes / sizeof(double); i++)
+                for (var i = 0; i < samplesToGet; i++)
                 {
                     rawDouble[i] = CurrentStream.ReadSingle();
                 }
 
-                sampleFrame = new DoubleSampleFrame(rawDouble, numSamples, Format.NumChannels, _format);
+                sampleFrame = new DoubleSampleFrame(rawDouble, samplesToGet, Format.NumChannels, AudioFormat);
                 break;
 
             default:
