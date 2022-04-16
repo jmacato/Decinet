2da770c02d9f1219d3f7d9b2d3c7afe6ea7b397b
Jumar Macato <16554748+jmacato@users.noreply.github.com>
2022-04-16 20:18:05 +0800

    implement short to float resampler

---
+++ Decinet/ShortToFloatResampler.cs
@@ -0,0 +1,48 @@
+﻿using System.Reflection.Metadata.Ecma335;
+using Decinet.Architecture;
+
+namespace Decinet;
+
+public class ShortToFloatResampler : IResampler
+{
+    private IBackend? _backend;
+    private IPlaybackController? _playbackController;
+    private IDSPStack? _dspStack;
+    private bool _isDisposed = false;
+
+    /// <inheritdoc />
+    public void Receive(ISampleFrame data)
+    {
+        if (_isDisposed && _backend is null) return;
+
+        var incomingFormat = data.AudioFormat;
+        var receivingFormat = _backend!.DesiredAudioFormat;
+
+        if (incomingFormat == receivingFormat)
+            _dspStack?.Receive(data);
+        
+        
+    }
+
+    /// <inheritdoc />
+    public void Connect(IPlaybackController priorNode, IDSPStack targetNode)
+    {
+        _playbackController = priorNode;
+        _dspStack = targetNode;
+    }
+
+    /// <inheritdoc />
+    public void ConnectBackend(IBackend backend)
+    {
+        _backend = backend;
+    }
+
+    /// <inheritdoc />
+    public void Dispose()
+    {
+        _isDisposed = true;
+        _backend = null!;
+        _playbackController = null!;
+        _dspStack = null!;
+    }
+}
